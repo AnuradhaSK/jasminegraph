734fc6b66af17fae377005428f4c54129026b24c
AnuradhaSK <anuradha199528@gmail.com>
2019-04-29 20:12:01 +0530

    Added training invoke implementation on workers

--- src/server/JasmineGraphServer.cpp
+++ src/server/JasmineGraphServer.cpp
@@ -1170,6 +1170,13 @@ int JasmineGraphServer::removePartitionThroughService(string host, int port, str
     return 0;
 }
 
+std::vector<JasmineGraphServer::workers> JasmineGraphServer::getHostWorkerMap() {
+    return hostWorkerMap;
+}
+
+
+
+
 void JasmineGraphServer::updateOperationalGraphList() {
     Utils utils;
     string hosts = "";
